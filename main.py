79c662b9548bba931fe0236371277591050d64e4
seungho Jin <khuj05@naver.com>
2026-01-28 02:43:54 +0900

    문제 00000번 풀이 완료 (test)

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
-M, N=map(int, input().split())
+import sys
 
-for i in range(M,N+1):
-    if i<2:
-        continue
+N=int(sys.stdin.readline())
+matrix=[[0]*N for _ in range(N)]
 
-    prime=True
-    for j in range(2, int(i**0.5)+1):
-        if i%j==0:
-            prime=False
+
+for i in range(N):
+    row=[0]*N
+    row=list(map(int, sys.stdin.readline().split()))
+    for j in range(N):
+        matrix[i][j]=row[j]
+
+white=int(0)
+blue=int(0)
+
+def decom(xs,ys,xe,ye):
+    global white, blue
+    temp_color=matrix[xs][ys]
+
+    exit_flag = False
+
+    for i in range(xs,xe):
+        if exit_flag==True:
             break
-    
-    if prime==True:
-        print(i)
-        
+        for j in range(ys,ye):
+            if matrix[i][j]!=temp_color:
+                exit_flag=True
+                break
+            else:
+                continue
+    if exit_flag==True:
+        if int(xs)==int(xe):
+            if temp_color==1:
+                blue+=1
+            else:
+                white+=1
+            return
+
+        else:
+            mid_x=(xs+xe)//2
+            mid_y=(ys+ye)//2
+            decom(xs,ys,mid_x,mid_y)
+            decom(xs,mid_y,mid_x,ye)
+            decom(mid_x,ys,xe,mid_y)
+            decom(mid_x,mid_y,xe,ye)
+    else:
+        if temp_color==1:
+            blue+=1
+        else:
+            white+=1
+        return
 
 
+decom(0,0,N,N)
+print(white)
+print(blue)
--- submit.sh
+++ submit.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-# 사용법: ./submit.sh [문제번호] [코멘트]
-# 예시: ./submit.sh 1000 "DP 풀이"
+# 사용법: ./submit.sh [문제번호] [폴더명] [코멘트]
+# 예시: ./submit.sh 1000 "DP" "피보나치 풀이"
 
 # 1. 입력값 확인
-if [ -z "$1" ]; then
-  echo "오류: 문제 번호를 입력해주세요!"
-  echo "사용법: ./submit.sh [문제번호]"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "오류: 문제 번호와 폴더명을 입력해주세요!"
+  echo "사용법: ./submit.sh [문제번호] [폴더명] [코멘트]"
   exit 1
 fi
 
 PROBLEM_ID=$1
-COMMENT=${2:-"문제 $1번 풀이 완료 (Python)"} # 코멘트 없으면 기본값
+FOLDER_NAME=$2
+COMMENT=${3:-"문제 $1번 풀이 완료 ($2)"} # 세 번째 인자가 코멘트가 됨
 
-# 2. 저장할 폴더가 없으면 생성 (solutions 폴더)
-mkdir -p solutions
+# 2. 계층 구조 폴더 생성 (solutions/폴더명)
+# -p 옵션은 상위 폴더가 없으면 알아서 다 만들어줍니다.
+TARGET_DIR="solutions/$FOLDER_NAME"
+mkdir -p "$TARGET_DIR"
 
-# 3. main.py를 해당 문제 번호.py로 복사
-cp main.py solutions/$PROBLEM_ID.py
+# 3. main.py를 해당 폴더 안의 문제 번호.py로 복사
+cp main.py "$TARGET_DIR/$PROBLEM_ID.py"
 
 # 4. Git 명령어로 업로드
-git add solutions/$PROBLEM_ID.py
+git add "$TARGET_DIR/$PROBLEM_ID.py"
 git commit -m "$COMMENT"
 git push
 
-echo "$PROBLEM_ID번(Python) 업로드 완료!"
+echo "[$FOLDER_NAME] $PROBLEM_ID번 업로드 완료!"
