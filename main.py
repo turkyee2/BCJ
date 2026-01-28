f699e0212fe3aec73eb531fec41b7799af3eae45
seungho Jin <khuj05@naver.com>
2026-01-28 23:54:57 +0900

    문제 2805번 풀이 완료 (class3)

--- main.py
+++ main.py
@@ -1,56 +1,43 @@
 import sys
 
-N=int(sys.stdin.readline())
-matrix=[[0]*N for _ in range(N)]
+N,M=map(int, sys.stdin.readline().split())
+Trees=[0]*N
+Trees=list(map(int, sys.stdin.readline().split()))
 
+max=0
 
 for i in range(N):
-    row=[0]*N
-    row=list(map(int, sys.stdin.readline().split()))
+    if Trees[i]>max:
+        max=Trees[i]
+#cur_max=max
+
+low=0
+high=max
+
+while high>low:
+    mid=(low+high)//2
+
+    sum=int(0)
     for j in range(N):
-        matrix[i][j]=row[j]
+        if Trees[j]>mid:
+            sum+=(Trees[j]-mid)
 
-white=int(0)
-blue=int(0)
-
-def decom(xs,ys,xe,ye):
-    global white, blue
-    temp_color=matrix[xs][ys]
-
-    exit_flag = False
-
-    for i in range(xs,xe):
-        if exit_flag==True:
-            break
-        for j in range(ys,ye):
-            if matrix[i][j]!=temp_color:
-                exit_flag=True
-                break
-            else:
-                continue
-    if exit_flag==True:
-        if int(xs)==int(xe):
-            if temp_color==1:
-                blue+=1
-            else:
-                white+=1
-            return
-
-        else:
-            mid_x=(xs+xe)//2
-            mid_y=(ys+ye)//2
-            decom(xs,ys,mid_x,mid_y)
-            decom(xs,mid_y,mid_x,ye)
-            decom(mid_x,ys,xe,mid_y)
-            decom(mid_x,mid_y,xe,ye)
+    if sum>=M:
+        result=mid
+        low=mid+1
     else:
-        if temp_color==1:
-            blue+=1
-        else:
-            white+=1
-        return
+        high=mid-1
 
+print(result)
+"""
 
-decom(0,0,N,N)
-print(white)
-print(blue)
+temp=M
+while temp>0:
+    for j in range(N):
+        if Trees[j]==cur_max:
+            Trees[j]-=1
+            temp-=1
+    cur_max-=1
+
+print(cur_max)
+"""
