3beb296ba9e37c6df896247f4a8e29038dba3884
seungho Jin <khuj05@naver.com>
2026-02-01 02:57:54 +0900

    문제 11724번 풀이 완료 (class3)

--- solutions/class3/11724.py
+++ solutions/class3/11724.py
@@ -1,5 +1,31 @@
 import sys
+from collections import deque
 
-N,M=input()
+N,M=map(int, sys.stdin.readline().split())
 
-print(N,M)
+edges=[[] for _ in range(N+1)]
+nodes=[1]*(N+1)
+
+for i in range(M):
+    a,b=map(int, sys.stdin.readline().split())
+    edges[a].append(b)
+    edges[b].append(a)
+conn=deque()
+
+result=0
+
+for j in range(1,N+1):
+    if nodes[j]==1:
+        result+=1
+        conn.append(j)
+        nodes[j]=0
+
+        while conn:
+            cur=conn.popleft()
+            for k in edges[cur]:
+                if nodes[k]==1:
+                    nodes[k]=0
+                    conn.append(k)
+
+            
+print(result)
